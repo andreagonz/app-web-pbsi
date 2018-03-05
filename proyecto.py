faaf1c96d261ceac6c83fdf93d7a50e0abdbb54f
andreagonz <andreagonz@ciencias.unam.mx>
2018-03-04 19:50:45 -0600

    Solo falta hacer la peticion de ajax

--- proyecto.py
+++ proyecto.py
@@ -26,24 +26,12 @@ Equipo:
     Pedro Ángel Valle Juárez
 '''
 
-# Agentes de usuario más comunes.
-user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0.1 Safari/604.3.5",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
-    "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0"
-]
-
-class MiVisitor(esprima.NodeVisitor):
+class VisitorAjax(esprima.NodeVisitor):
 
     def __init__(self, lst):
         self.lst = lst
         self.url = ""
-        super(MiVisitor, self).__init__()
+        super(VisitorAjax, self).__init__()
         
     def visit_CallExpression(self, node):
         ob = node.callee.object.name if node.callee.object else ''
@@ -51,7 +39,21 @@ class MiVisitor(esprima.NodeVisitor):
         if prop == 'ajax' and (ob == 'jQuery' or ob == '$'):
             self.lst.append((self.url, node))
         self.generic_visit(node)
-    
+
+class VisitorData(esprima.NodeVisitor):
+
+    def __init__(self, data):
+        self.data = data
+        self.resultado = None
+        super(VisitorAjax, self).__init__()
+        
+    def visit_CallExpression(self, node):
+        ob = node.callee.object.name if node.callee.object else ''
+        prop = node.callee.property.name if node.callee.property else ''
+        if prop == 'ajax' and (ob == 'jQuery' or ob == '$'):
+            self.lst.append((self.url, node))
+        self.generic_visit(node)
+
 def opciones():
     """
     Regresa un objeto que permite leer argumentos ingresados al ejecutar el programa.
@@ -59,11 +61,9 @@ def opciones():
         optparse.OptionParser - Objeto analizador de argumentos
     """
     parser = optparse.OptionParser()
-    parser.add_option('-a','--agente-usuario', dest='agente', default=None, help='Agente de usuario a ser utilizado para las peticiones.')
-    parser.add_option('-p','--proxy', dest='proxy', default=None, help='')
-    parser.add_option('-u','--uri', dest='uri', default=None, help='URI del recurso a inspeccionar.')
-    opts, args = parser.parse_args()
-    return opts
+    parser.add_option('-c','--config', dest='config', default=None, help='Archivo de configuracion a ser utilizado.')
+    parser.add_option('-a','--archivo', dest='archivo', default=False, action='store_true', help='Bandera que de ser usada, indica que la entrada del programa es un archivo.')
+    return parser.parse_args()
 
 def error(msg, exit=False):
     """
@@ -92,6 +92,42 @@ def obten_src(url, src):
         return urljoin(url, src)
     return src
 
+def peticion_ajax(t, sesion, agente, cookie, mostrar_respuesta, mostrar_funciones_asincronas):
+    """
+    Hace una petición usando una tupla de la forma (url, ajax), donde url
+    es el recurso de donde se extrajo la función de ajax y ajax es la definición
+    de la función que permite realizar peticiones asíncronas.    
+    Recibe:
+        t (tuple) - Tupla de la forma (string, esprima.Node)
+    """
+    url = t[0]
+    ajax = t[1]
+    metodo = 'GET'
+    data = None
+    contentType = 'application/x-www-form-urlencoded; charset=UTF-8'
+    for prop in ajax.arguments[0].properties:
+        if prop.key.name == 'type' or prop.key.name == 'method':
+            metodo = prop.value.value if prop.value.type == "Literal" else metodo
+        elif prop.key.name == 'url':
+            url = obten_src(url, prop.value.value) if prop.value.type == "Literal" else url
+        elif prop.key.name == 'contentType':
+            contentType = prop.value.value if prop.value.type == "Literal" else contentType
+        elif prop.key.name == 'data':
+            data = prop.value.value if prop.value.type == "Literal" else None
+            if data is None:
+                data = prop.value
+    print "\n------------------------------\n"
+    print "Recurso: %s" % url
+    if mostrar_funciones_asincronas:
+        print "Funcion:"
+        print ajax
+
+    print metodo
+    print contentType
+    # print data if not data is None else ''
+    if mostrar_respuesta:
+        pass
+    
 def obten_ajax(lst):
     """
     A partir de una lista de tuplas de la forma (url, javascript), regresa una
@@ -102,7 +138,7 @@ def obten_ajax(lst):
         list - Lista de tuplas de la forma (string, esprima.Node)
     """
     ajax = []
-    visitor = MiVisitor(ajax)
+    visitor = VisitorAjax(ajax)
     for js in lst:
         if not js[1] is None:
             try:
@@ -160,7 +196,7 @@ def obten_sesion(proxy=None):
         sesion.proxies = {'http': proxy_http, 'https': proxy_https}
     return sesion
 
-def hacer_peticion(url, sesion, agente, cookie):
+def hacer_peticion(url, sesion, agente=None, cookie=None, contentType=None, data=None):
     """
     Hace una petición al servidor.
     Recibe:
@@ -172,24 +208,66 @@ def hacer_peticion(url, sesion, agente, cookie):
         requests.models.Response - Respuesta a la petición
     """
     try:
+        headers = {}
         if agente is not None:
-            headers = {'User-Agent': agente}
-            return sesion.get(url, headers=headers, verify=False)
-        return sesion.get(url, verify=False)
+            headers['User-Agent'] = agente
+        if cookie is not None:
+            headers['Cookie'] = cookie
+        if contentType is not None:
+            headers['Content-Type'] = contentType
+        return sesion.get(url, headers=headers, verify=False)
     except ConnectionError as e:
         error('Error en la conexion: ' + str(e), True)
     return None
 
+def leer_configuracion(archivo):
+    """
+    Lee el archivo de configuración y regresa un diccionario con los valores que éste indica.
+    Recibe:
+        archivo (str) - Nombre del archivo de configuración
+    Regresa:
+        dict - Diccionario con los valores de los parámetros
+    """
+    res = {}
+    try:
+        config = configparser.ConfigParser()
+        config.read(archivo)
+        res = config['CONFIGURACION']
+    except:
+        error("Hubo un problema al leer el archivo de configuración", True)
+    return res
+
 if __name__ == '__main__':
     # try:
     urllib3.disable_warnings()
-    ops = opciones()
-    sesion = obten_sesion(getattr(ops, 'proxy', None))
-    url = genera_url(ops.uri)
-    peticion = hacer_peticion(url, sesion, None, "")
-    ajax = obten_ajax(obten_js(url, peticion.content, sesion, None, ""))
+    ops, args = opciones()
+    if len(args) < 1:
+        error("Uso: python proyecto.py <uri|archivo> [-a] [-c archivo.conf]", True)
+    config = None if ops.config is None else leer_configuracion(ops.config)
+    proxy = None
+    agente = None
+    cookie = None
+    mostrar_respuesta = None
+    mostrar_funciones_asincronas = False
+    if not config is None:
+        proxy = config.get('proxy', None)
+        agente = config.get('user_agent', None)
+        cookie = config.get('cookie', None)
+        mostrar_respuesta = config.get('mostrar_respuesta', None)
+        mostrar_funciones_asincronas = config.getboolean('mostrar_funciones_asincronas', fallback=False)
+        print "Configuraciones:"
+        print "	Proxy: %s" % ('Ninguno' if proxy is None else proxy)
+        print "	Agente de usuario: %s" % ('Ninguno' if agente is None else agente)
+        print "	Cookie: %s" % ('Ninguno' if cookie is None else cookie)
+        print "	Mostrar respuesta: %s" % ('Ninguna' if mostrar_respuesta is None else mostrar_respuesta)
+        print "	Mostrar funciones asíncronas: %s" % str(mostrar_funciones_asincronas)
+    sesion = obten_sesion(proxy)
+    url = genera_url(args[0])
+    peticion = hacer_peticion(url, sesion, agente, cookie)
+    ajax = obten_ajax(obten_js(url, peticion.content, sesion, agente, cookie))
     for x in ajax:
-        print x
+        peticion_ajax(x, sesion, agente, cookie, mostrar_respuesta, mostrar_funciones_asincronas)
+
     # except Exception as e:
         # error('Ocurrió un error inesperado')
         # error(e, True)
